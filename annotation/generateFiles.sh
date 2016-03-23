d8df21c2ddfc4d3d782beb67372b596799db5076
Charles Plessy <plessy@riken.jp>
2016-03-23 17:58:52 +0900

    Correct the format of the annotation file.

--- annotation/generateFiles.sh
+++ annotation/generateFiles.sh
@@ -12,31 +12,18 @@ zgrep -v '#' gencode.v14.annotation.gtf.gz |
 GENCODE=gencode.v14.annotation.gtf.gz
 
 zcat $GENCODE |
-  grep -P '\tgene\t' |
-  perl -F'\t' -anE 'say join "\t", @F[0], @F[3] - 1, @F[4], join ("_", @F[8] =~ /transcript_type "(.*?)"/, @F[8] =~ /transcript_name "(.*?)"/), 0, @F[6]' > annot.bed
-
-zcat $GENCODE |
   grep -P '\texon\t' |
   perl -F'\t' -anE 'say join "\t", @F[0], @F[3] - 1, @F[4], "exon", 0, @F[6]' >> annot.bed
 
 zcat $GENCODE |
-  grep -P '\texon\t' |
-  awk '{OFS="\t"}
-    {print $1, $4 - 10, $4 + 10, "boundary", 0, $7}
-    {print $1, $5 - 10, $5 + 10, "boundary", 0, $7}' >> annot.bed
+  grep -P '\tgene\t' |
+  perl -F'\t' -anE 'say join "\t", @F[0], @F[3] - 1, @F[4], "gene", 0, @F[6]' >> annot.bed
 
 zcat $GENCODE |
   grep -P '\tgene\t' |
   awk '{OFS="\t"} {
-      if ($7=="+") {print $1,$4 - 100,$4 + 100,"TSS",0,$7}
-      if ($7=="-") {print $1,$5 - 100,$5 + 100,"TSS",0,$7}
-    }' >> annot.bed
-
-zcat $GENCODE |
-  grep -P '\tgene\t' |
-  awk '{OFS="\t"} {
-      if ($7=="+") {print $1,$5 - 100,$5 + 100,"TTS",0,$7}
-      if ($7=="-") {print $1,$4 - 100,$4 + 100,"TTS",0,$7}
+      if ($7=="+") {print $1,$4 - 100,$4 + 100,"promoter",0,$7}
+      if ($7=="-") {print $1,$5 - 100,$5 + 100,"promoter",0,$7}
     }' >> annot.bed
 
 md5sum -c md5sums.txt 
